219abf945a39ba7c8772f046862179f0721a9087
DropXpert <56500369+DropXpert@users.noreply.github.com>
2025-01-29 15:27:27 +0530

    index.js

--- index.js
+++ index.js
@@ -131,11 +131,11 @@ function generateBrowserId(index) {
 function displayHeader() {
   const width = process.stdout.columns;
   const headerLines = [
-    "<|============================================|>",
-    "                Teneo Bot NEW                   ",
-    "     https://github.com/0xZxcrypto/Teneoxyz     ",
-    "<|============================================|>"
-  ];
+    <|============================================|>
+                    Teneo Bot UPDATED                   
+               https://github.com/DropXpert     
+               https://t.me/DropXpert5
+    <|============================================|>
 
   console.log("");
   headerLines.forEach(line => {
